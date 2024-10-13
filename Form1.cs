d342f40441efad089a156668ff4fc41c454ee249
cmliu <admin@cmliu.net>
2024-10-13 17:51:57 +0800

    添加: 1.添加 CFnat-Windows-GUI免责声明; 修复: 1.调整windows7模式下的窗口高度,避免显示不全;

--- Form1.cs
+++ Form1.cs
@@ -17,7 +17,7 @@ namespace cfnat.win.gui
     {
         private Process cmdProcess;
         private NotifyIcon notifyIcon;
-
+        private bool isExitingDueToDisclaimer = false;
         public Form1()
         {
             InitializeComponent();
@@ -90,19 +90,19 @@ namespace cfnat.win.gui
         // 修改 Form1_FormClosing 方法
         private async void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
-            if (e.CloseReason == CloseReason.UserClosing)
+            if (e.CloseReason == CloseReason.UserClosing && !isExitingDueToDisclaimer)
             {
-                e.Cancel = true; // 取消关闭操作
-                Hide(); // 隐藏窗体
-                notifyIcon.Visible = true; // 显示系统托盘图标
+                e.Cancel = true;
+                Hide();
+                notifyIcon.Visible = true;
             }
             else
             {
-                e.Cancel = true; // 暂时取消关闭操作
-                await StopCommandAsync(); // 停止命令进程
-                e.Cancel = false; // 允许关闭
-                notifyIcon.Dispose(); // 释放 NotifyIcon 资源
-                Application.Exit(); // 确保应用程序完全退出
+                e.Cancel = true;
+                await StopCommandAsync();
+                e.Cancel = false;
+                notifyIcon.Dispose();
+                Application.Exit();
             }
         }
 
@@ -248,6 +248,7 @@ namespace cfnat.win.gui
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
+            if (button3.Text == "高级设置∧") button3_Click(sender, e);
             // 判断 comboBox1 的选中的文本
             if (comboBox1.Text.Equals("windows", StringComparison.OrdinalIgnoreCase))
             {
@@ -260,6 +261,7 @@ namespace cfnat.win.gui
                 comboBox2.Items.Add("arm");
                 comboBox2.Items.Add("arm64");
                 comboBox2.Text = "amd64";
+                this.Height = 463;
             }
             else
             {
@@ -269,6 +271,7 @@ namespace cfnat.win.gui
                 comboBox2.Items.Add("386");
                 comboBox2.Items.Add("amd64");
                 comboBox2.Text = "amd64";
+                this.Height = 493;
             }
         }
         private void textBox1_Leave(object sender, EventArgs e)
@@ -499,7 +502,17 @@ namespace cfnat.win.gui
             else
             {
                 // 文件不存在，可以给用户反馈
-                MessageBox.Show("配置文件未找到，将使用默认设置。", "信息", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                string 免责声明 = "CFnat-Windows-GUI项目仅供教育、研究和安全测试目的而设计和开发。本项目旨在为安全研究人员、学术界人士及技术爱好者提供一个探索和实践网络通信技术的工具。\r\n在下载和使用本项目代码时，使用者必须严格遵守其所适用的法律和规定。使用者有责任确保其行为符合所在地区的法律框架、规章制度及其他相关规定。\r\n\r\n使用条款\r\n\r\n教育与研究用途：本软件仅可用于网络技术和编程领域的学习、研究和安全测试。\r\n禁止非法使用：严禁将CFnat-Windows-GUI用于任何非法活动或违反使用者所在地区法律法规的行为。\r\n使用时限：基于学习和研究目的，建议用户在完成研究或学习后，或在安装后的24小时内，删除本软件及所有相关文件。\r\n免责声明：CFnat-Windows-GUI的创建者和贡献者不对因使用或滥用本软件而导致的任何损害或法律问题负责。\r\n用户责任：用户对使用本软件的方式以及由此产生的任何后果完全负责。\r\n无技术支持：本软件的创建者不提供任何技术支持或使用协助。\r\n知情同意：使用CFnat-Windows-GUI即表示您已阅读并理解本免责声明，并同意受其条款的约束。\r\n\r\n请记住：本软件的主要目的是促进学习、研究和安全测试。创作者不支持或认可任何其他用途。使用者应当在合法和负责任的前提下使用本工具。\r\n\r\n同意以上条款请点击\"是 / Yes\"，否则程序将退出。";
+
+                // 显示带有 "同意" 和 "拒绝" 选项的对话框
+                DialogResult result = MessageBox.Show(免责声明, "CFnat-Windows-GUI 免责声明", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                // 如果用户点击 "拒绝" (对应于 No 按钮)
+                if (result == DialogResult.No)
+                {
+                    // 退出程序
+                    Environment.Exit(0); // 立即退出程序
+                }
             }
         }
 
@@ -597,13 +610,15 @@ namespace cfnat.win.gui
             if (button3.Text== "高级设置∨") {
                 groupBox3.Visible = true; 
                 button3.Text = "高级设置∧";
-                this.Height = 546;
+                //this.Height = 546;
+                this.Height += 83; 
             }
             else
             {
                 groupBox3.Visible = false;
                 button3.Text = "高级设置∨";
-                this.Height = 463;
+                //this.Height = 463;
+                this.Height -= 83;
             }
         }
 
