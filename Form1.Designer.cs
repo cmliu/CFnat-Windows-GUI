a4371ae827e2037a39db41d053ba9f3d4dfa33bc
cmliu <admin@cmliu.net>
2024-10-18 02:57:13 +0800

    ?

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -357,7 +357,7 @@
             // timer2
             // 
             this.timer2.Enabled = true;
-            this.timer2.Interval = 6000;
+            this.timer2.Interval = 60000;
             this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
             // 
             // label8
